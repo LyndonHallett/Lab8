25e3561799048f3c1720d705e45544a6ecbcca5d
Lyndon Hallett <lhallett@ualberta.ca>
2023-03-05 14:34:26 -0700

    Add test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,4 +13,15 @@ import java.util.ArrayList;
 public class CustomListTest {
 
 
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus one
+     */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount(); list.addCity(new City("Estevan", "SK")); assertEquals(list.getCount(),listSize + 1);
+    }
+
 }
