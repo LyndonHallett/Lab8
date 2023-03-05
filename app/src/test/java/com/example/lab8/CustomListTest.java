b0e197c48332c1714b8386a07923acc0cb9fa51b
Lyndon Hallett <lhallett@ualberta.ca>
2023-03-05 15:34:52 -0700

    Add hasCityTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,10 +1,12 @@
 package com.example.lab8;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 //import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +24,22 @@ public class CustomListTest {
     public void addCityTest(){
         CustomList list = new CustomList(null, new ArrayList<>());
         int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SK")); assertEquals(list.getCount(),listSize + 1);
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+
+    /**
+     * Test if the list has a provided city by checking if it exists in the list after adding
+     * it to the list. Before adding the city to the list, the method should indicate it is
+     * not in the list
+     */
+    @Test
+    public void hasCityTest() {
+        CustomList list = new CustomList(null, new ArrayList<>());
+        City city = new City("Estevan", "SK");
+        assertFalse(list.hasCity(city));
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
     }
 
 }
